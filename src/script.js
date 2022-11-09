d83156fcbec24ddcd7cb878aeeab77072a1f0c95
CookiCode <cookikit@gmail.com>
2022-11-09 20:32:53 +1300

    Update! :)

--- src/script.js
+++ src/script.js
@@ -1,3 +1,24 @@
+const settings = document.getElementById("settings");
+const color = document.getElementById("color");
+const canvas = document.getElementById("canvas");
+c = canvas.getContext("2d");
+
+c.fillStyle = "#fff"
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+if (getParameterByName('bg-color') != null) {
+    if ((String(getParameterByName('bg-color')).charAt(0) == "r")) {
+        c.fillStyle = getParameterByName('bg-color');
+    } else {
+        c.fillStyle = "#"+getParameterByName('bg-color');
+    }
+}
+
+c.beginPath();
+c.moveTo(0, canvas.height);
+c.quadraticCurveTo(canvas.width/2, canvas.height/2, canvas.width, canvas.height);
+c.fill();
+
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
     var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
@@ -8,14 +29,17 @@ function getParameterByName(name, url = window.location.href) {
 }
 
 function redirect() {
-    window.location.replace("?bg-color="+document.getElementById("color").value.slice(1));
+    window.location.replace("?bg-color="+color.value.slice(1));
 }
 
-if (getParameterByName('bg-color').charAt(0) == r) {
-    window.addEventListener("load",function() { document.body.style.background = getParameterByName('bg-color') });
-    window.addEventListener("load",function() { document.getElementById("color").value = getParameterByName('bg-color') });
-} else {
-    window.addEventListener("load",function() { document.body.style.background = "#"+getParameterByName('bg-color') });
-    window.addEventListener("load",function() { document.getElementById("color").value = "#"+getParameterByName('bg-color') });
+if (getParameterByName('bg-color') != null) {
+    if ((String(getParameterByName('bg-color')).charAt(0) == "r")) {
+        window.addEventListener("load",function() { document.body.style.background = getParameterByName('bg-color') });
+        window.addEventListener("load",function() { color.value = getParameterByName('bg-color') });
+    } else {
+        window.addEventListener("load",function() { document.body.style.background = "#"+getParameterByName('bg-color') });
+        window.addEventListener("load",function() { color.value = "#"+getParameterByName('bg-color') });
+    }
 }
-document.getElementById("settings").style.paddingTop = window.innerHeight+"px";
+
+settings.style.paddingTop = window.innerHeight+"px";
