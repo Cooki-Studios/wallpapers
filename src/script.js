a0d840598c7f860bc1b823846e32c1ff5f4495d2
CookiCode <cookikit@gmail.com>
2022-11-10 16:50:13 +1300

    Update

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ const color = document.getElementById("bg-color");
 const color2 = document.getElementById("fill-style");
 const amount = document.getElementById("amount");
 
-if (getParameterByName('bg-color') != null) {
+if (getParameterByName('amount') != null) {
     amount.value = getParameterByName('amount');
 }
 
@@ -16,15 +16,19 @@ canvas.height = window.innerHeight;
 if (getParameterByName('fill-style') != null) {
     if ((String(getParameterByName('fill-style')).charAt(0) == "r")) {
         c.fillStyle = getParameterByName('fill-style');
+        color2.value = getParameterByName('fill-style');
     } else {
         c.fillStyle = "#"+getParameterByName('fill-style');
+        color2.value = "#"+getParameterByName('fill-style');
     }
 }
 
-c.beginPath();
-c.moveTo(0, canvas.height);
-c.quadraticCurveTo(canvas.width/2, Math.random()*canvas.height/2, canvas.width/amount.value, canvas.height);
-c.fill();
+for (let i = 0; i < amount.value; i++) {
+    c.beginPath();
+    c.moveTo(canvas.width/amount.value*i, canvas.height);
+    c.quadraticCurveTo(canvas.width/amount.value*i/3, Math.random()*canvas.height/2, canvas.width/amount.value*(i+1), canvas.height);
+    c.fill();
+}
 
 function getParameterByName(name, url = window.location.href) {
     name = name.replace(/[\[\]]/g, '\\$&');
@@ -43,8 +47,10 @@ if (getParameterByName('bg-color') != null) {
     if ((String(getParameterByName('bg-color')).charAt(0) == "r")) {
         window.addEventListener("load",function() { document.body.style.background = getParameterByName('bg-color') });
         window.addEventListener("load",function() { color.value = getParameterByName('bg-color') });
+        color.value = "#"+getParameterByName('bg-color');
     } else {
         window.addEventListener("load",function() { document.body.style.background = "#"+getParameterByName('bg-color') });
         window.addEventListener("load",function() { color.value = "#"+getParameterByName('bg-color') });
+        color.value = getParameterByName('bg-color');
     }
 }
